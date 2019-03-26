ec9b3f1eca3846b5b99644a25d6dd7bd508360ea
kristian ugahari <kristianugahari@gmail.com>
2019-03-26 14:54:59 +0700

    memberclass

---
+++ Taxi.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass
+{
+    class Taxi
+    {
+        //properties
+        public string DriverName { get; set; }
+        public string PassName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        //method
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name : {0}", DriverName);
+            Console.WriteLine("On Duty : {0}", OnDuty ? "No" : "Yes");
+            Console.WriteLine("Number of Passenger : {0}", NumPassenger);
+            Console.WriteLine("Passenger Name : {0}\n", PassName);
+        }
+
+        public void PickUpPassenger() 
+        {
+            Console.WriteLine("{0} sedang menjemput {1}", DriverName, PassName);
+        }
+
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} selesai mengantar {1}", DriverName, PassName);
+            Console.WriteLine();
+        }
+    }
+}
